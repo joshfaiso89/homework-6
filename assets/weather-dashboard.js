5fa5e9811662e1660cbaa3dceae697a87a3f466f
Josh Faison <Joshfaiso@yahoo.com>
2019-12-21 18:29:50 -0500

    intializing json response into a function to display users input

--- assets/weather-dashboard.js
+++ assets/weather-dashboard.js
@@ -1,22 +1,57 @@
+//global variables
 var apiKey = '4e5dbe7db2b5e9c8b47fa40b691443d5'; 
-var cityURL = 'https://api.openweathermap.org/data/2.5/weather?appid=4e5dbe7db2b5e9c8b47fa40b691443d5&q=raleigh';
+var city = 'raleigh'; 
+var currentConditions = 'https://api.openweathermap.org/data/2.5/weather?appid='; 
 var fiveDay = 'https://api.openweathermap.org/data/2.5/forecast?4e5dbe7db2b5e9c8b47fa40b691443d5q={city name},{country code}';
 var uvIndex = 'https://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}';
+var searchedArr = JSON.parse(localStorage.getItem('searchedItems')) || [];
 
-
-
-$('button').on('click', function(event){
-  var input = $('input').val()
-  console.log(input);
-})
-
-
+//taking in user input, and passing the value into a variable
 $(document).ready(function(){
-  $.ajax({
-    url: cityURL,
-    method: "Get"
-  }).then(function(data){
-    console.log(data);
+  $('#search-input').on('click', function (event){
+    var userInput = $('#city-search').val();
+    console.log(userInput);
+    getWeather(userInput);
   })
 })
+
+function getWeather(cityName){
+  var apiCall = '';
+
+  if (cityName !== ''){ 
+    apiCall = currentConditions + apiKey + '&q=' + cityName; 
+    //return apiCall;
+  } else {
+    apiCall = currentConditions + apiKey + '&q=' + city; 
+    //return apiCall;
+  }
+
+  $.ajax({
+    url: apiCall,
+    method: "Get"
+  }).then(function(response){
+    console.log(response);
+    var feelslike = response.main.feels_like; 
+    $('#current-weather').append('<div>' + feelslike + '</div>');
+
+  })
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+      
+  
+
+
+
+
 
