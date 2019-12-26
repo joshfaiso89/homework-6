3f491f1cf51b7c6211bb09190f426715e2947b5a
Josh Faison <Joshfaiso@yahoo.com>
2019-12-25 20:00:01 -0500

    appended users input to a list

--- assets/weather-dashboard.js
+++ assets/weather-dashboard.js
@@ -5,15 +5,27 @@ var currentConditions = 'https://api.openweathermap.org/data/2.5/weather?appid='
 var fiveDay = 'https://api.openweathermap.org/data/2.5/forecast?4e5dbe7db2b5e9c8b47fa40b691443d5q={city name},{country code}';
 var uvIndex = 'https://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}';
 var searchedArr = JSON.parse(localStorage.getItem('searchedItems')) || [];
+var searchInput = document.querySelector('#search-input');
+var ul = document.querySelector('#list');
 
+var btn = document.querySelector('.button');
 //taking in user input, and passing the value into a variable
 $(document).ready(function(){
   $('#search-input').on('click', function (event){
-    var userInput = $('#city-search').val();
-    console.log(userInput);
+    let userInput = $('#city-search').val();
+    //console.log(userInput);
     getWeather(userInput);
   })
 })
+
+//storing previous search history
+$('#search-input').click(function() {  
+  $('#list').append('<li>' + $('#city-search').val());
+});
+
+
+
+
 
 function getWeather(cityName){
   var apiCall = '';
@@ -30,12 +42,21 @@ function getWeather(cityName){
     url: apiCall,
     method: "Get"
   }).then(function(response){
-    console.log(response);
+    //console.log(response);
     var feelslike = response.main.feels_like; 
     $('#current-weather').append('<div>' + feelslike + '</div>');
+    
+    var name = response.sys.name;
+    $('#current-weather').append('<div>' + name + '</div>');
 
+    var temp = response.main.temp;
+    $('#current-weather').append('<div>' + temp + '</div>');
+
+    
   })
 }
+
+
     
 
 
