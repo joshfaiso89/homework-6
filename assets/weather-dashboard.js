8958d72d4b0f7dca8615e9027f1371488ebcd598
Josh Faison <Joshfaiso@yahoo.com>
2019-12-28 16:25:35 -0500

    added cards and 5 day api call

--- assets/weather-dashboard.js
+++ assets/weather-dashboard.js
@@ -5,28 +5,16 @@ var currentConditions = 'https://api.openweathermap.org/data/2.5/weather?appid='
 var fiveDay = 'https://api.openweathermap.org/data/2.5/forecast?4e5dbe7db2b5e9c8b47fa40b691443d5q={city name},{country code}';
 var uvIndex = 'https://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}';
 var searchedArr = JSON.parse(localStorage.getItem('searchedItems')) || [];
-var searchInput = document.querySelector('#search-input');
-var ul = document.querySelector('#list');
 
-var btn = document.querySelector('.button');
 //taking in user input, and passing the value into a variable
 $(document).ready(function(){
   $('#search-input').on('click', function (event){
-    let userInput = $('#city-search').val();
-    //console.log(userInput);
+    var userInput = $('#city-search').val();
+    console.log(userInput);
     getWeather(userInput);
   })
 })
-
-//storing previous search history
-$('#search-input').click(function() {  
-  $('#list').append('<li>' + $('#city-search').val());
-});
-
-
-
-
-
+// userInput is passed into the getWeather function as arguement 'cityName' 
 function getWeather(cityName){
   var apiCall = '';
 
@@ -40,25 +28,73 @@ function getWeather(cityName){
 
   $.ajax({
     url: apiCall,
-    method: "Get"
+    method: "GET"
   }).then(function(response){
-    //console.log(response);
-    var feelslike = response.main.feels_like; 
+    console.log(response);
+    var feelslike = response.main.temp; 
+    feelslike = ((feelslike-273.15)*1.8)+32;
+    feelslike = Math.floor(feelslike);
+    city = response.name; 
     $('#current-weather').append('<div>' + feelslike + '</div>');
-    
-    var name = response.sys.name;
-    $('#current-weather').append('<div>' + name + '</div>');
+     $('#current-weather').append('<div>' + city + '</div>');
+ fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+ $.ajax({
+      url: fiveDay,
+      method: "GET"
+  }).
+  then(function(response){
+    console.log(response);
 
-    var temp = response.main.temp;
-    $('#current-weather').append('<div>' + temp + '</div>');
 
+   var averageTemp = 0; 
+   var previousdate = "";
+   var count = 0; 
+   var results = 0;
+   previousdate=moment().format("MM/DD/YYYY");
+  for (let index = 0; index < response.list.length; index++) {
     
+    var currentDate=moment(response.list[index].dt,"X").format("MM/DD/YYYY");
+    var temp=response.list[index].main.temp;
+    temp = ((temp-273.15)*1.8)+32;
+    temp = Math.floor(temp);
+    console.log(currentDate)
+    console.log(temp)
+    
+    
+    if(previousdate === currentDate){
+      averageTemp = averageTemp + temp; 
+      count++
+      previousdate = currentDate;
+    }
+    else {
+          results= averageTemp / count;
+          results = Math.floor(results);
+          console.log("results:",results)
+          var card = $("<div class = 'card col-sm-2'>")
+
+          var div1=$("<div class= 'card-header'>")
+          div1.append("Date:" + currentDate)
+          card.append(div1)
+
+          var div2=$("<div class= 'card-body'>")
+          div2.append("Average Temperature:" + results)
+          card.append(div2)
+          
+          $("#five-day").append(card);
+          
+
+          count = 0;
+          averageTemp = 0;
+          previousdate = currentDate;
+    }
+
+
+  }
+  
   })
-}
-
-
+})
     
-
+}
 
 
 
